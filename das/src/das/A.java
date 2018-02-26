2789f2f67f6f9c586db0b49cac55da40e488bcb6
derekbragado <BragadoD1394@sduhsd.lan>
2018-02-26 08:05:48 -0800

    unit7

---
+++ das/src/das/A.java
@@ -0,0 +1,12 @@
+package das;
+
+public class A {
+	String output=&quot;&quot;;
+	for(int i=1; i&lt;=4; i++){
+	for(int j=1; j&lt;=i; j++)
+	output=output+&#39;$&#39;;
+	output=output+&#39;\n&#39;;
+	}
+	out.println(output);
+
+}
