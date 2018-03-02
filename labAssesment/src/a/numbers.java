0237c7f818c2d58ef6a9a5e09d88164d4f6f2a0f
derekbragado <BragadoD1394@sduhsd.lan>
2018-03-02 09:21:36 -0800

    unit9

---
+++ labAssesment/src/a/numbers.java
@@ -0,0 +1,28 @@
+package a;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class numbers {
+	
+
+	
+	
+	public static boolean isGoofy(int num){
+		
+
+			
+		
+	
+			
+			
+		
+		}
+	
+	
+	public static int[] getSomeGoofyNumbers(int count){
+		isGoofy.getSomeGoofyNumbers;
+	}
+}
+
+
+
